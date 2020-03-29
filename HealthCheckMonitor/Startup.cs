19ebea848a2d61f89de009e30ab71be43d417444
ysferms <ermisyusuf@gmail.com>
2020-03-29 15:38:41 +0300

    test

--- HealthCheckMonitor/Startup.cs
+++ HealthCheckMonitor/Startup.cs
@@ -17,6 +17,7 @@ namespace HealthCheckMonitor
     public Startup(IConfiguration configuration)
     {
       Configuration = configuration;
+      //
     }
 
     public IConfiguration Configuration { get; }
